8bf67491cc25c40c0e455273fee83dfe2a128df7
RightSektor <vutalia89@gmail.com>
2023-09-18 18:40:20 +0200

    home__work--fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,14 @@
-let header = document.querySelector(".header__inner");
-let headerH = document.querySelector(".header__inner").clientHeight;
+let header = document.querySelector(".header__container");
+let headerH = document.querySelector(".header__container").clientHeight;
 
 console.log(headerH);
 
 document.onscroll = function () {
   let scroll = window.scrollY;
   if (scroll > headerH) {
-    header.classList.add("header--fixed");
-    document.body.style.paddingTop = headerH + "px";
+    header.classList.add("header__container--fixed");
   } else {
-    header.classList.remove("header--fixed");
-    document.body.removeAttribute("style");
+    header.classList.remove("header__container--fixed");
   }
 };
 
@@ -20,15 +18,19 @@ document.addEventListener("DOMContentLoaded", function () {
   menuLinks.forEach((link) => {
     link.addEventListener("click", function (e) {
       e.preventDefault();
-
-      // Удаляем класс "active" у всех ссылок
       menuLinks.forEach((item) => {
         item.classList.remove("active");
       });
 
-      // Добавляем класс "active" к текущей кликнутой ссылке
       this.classList.add("active");
     });
+  });
+});
+
+$(function () {
+  $(".review__slider").slick({
+    arrows: true,
+    dots: true,
   });
 });
 var mixer = mixitup(".popular__list, menu__list");
